bbcd2ea232666732dd05da216966a3fe03ac9253
mansiagar <73584623+mansiagar@users.noreply.github.com>
2024-12-30 15:49:45 +0530

    Update navbar.js

--- Script/navbar.js
+++ Script/navbar.js
@@ -1,10 +1,10 @@
 let nav = document.getElementById("nav");
 let navbar = () => {
   let navigation = `<div id="navbar">
-      <a href="./index.html">Index</a>
-      <a href="./admin.html">Admin</a>
+      <a href="index.html">Index</a>
+      <a href="admin.html">Admin</a>
 
-      <a href="./books.html">Books</a>
+      <a href="books.html">Books</a>
     </div>`;
   nav.innerHTML = navigation;
 };
